2ec3a21478cc314f48747a4beb3075e1180909e2
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-06-18 15:04:31 -0600

    add code 2410

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import {
   GoabAppFooter,
   GoabPageBlock,
   GoabAppFooterMetaSection,
-  GoabOneColumnLayout,
+  GoabOneColumnLayout, GoabButtonGroup, GoabButton, GoabModal,
 } from '@abgov/react-components';
+import {useState} from "react";
 
 export function App() {
+  const [open, setOpen] = useState(false);
+
+  function onClick() {
+    setOpen(true);
+  }
+
+  function onClose() {
+    console.log("closed");
+    setOpen(false);
+  }
   return (
     <GoabOneColumnLayout>
       <section slot="header">
@@ -24,7 +35,42 @@ export function App() {
       </section>
 
       <GoabPageBlock width="100%">
-        {/* Add content here */}
+        <GoabButton onClick={onClick}>Show Modal</GoabButton>
+        <GoabModal heading="Industry Description" open={open}>
+          <h4>Business, building, and other support services</h4>
+          <p>This industry is made up of two major sub industries:</p>
+          <ol>
+            <li>Management of companies and enterprises</li>
+            <li>Administrative and support, waste management and remediation services</li>
+          </ol>
+          <h4>Management of companies and enterprises</h4>
+          <p>This industry compprises:</p>
+          <ol>
+            <li>
+              Legal entities engaged only in holding the securities of subsidiaries and
+              affiliates for the purpose of owning a controlling interest or influencing the
+              management decisions of these businesses, or
+            </li>
+            <li>
+              Businesses primarily engaged in adminstering, overseeing, and managing other
+              establishments of the company or enterprise and which may also hold the
+              securities of their subsidiaries and affiliates.
+            </li>
+          </ol>
+          <h4>Administrative and support, waste management and remediation services</h4>
+          <p>This industry comprises of:</p>
+          <ol>
+            <li>
+              Administrative and support services, estabilishments primarily engaged in
+              activities such as administration, hiring, and placing personnel,{" "}
+            </li>
+          </ol>
+          <GoabButtonGroup alignment="end" mt="l">
+            <GoabButton type="primary" onClick={onClose}>
+              I understand
+            </GoabButton>
+          </GoabButtonGroup>
+        </GoabModal>
       </GoabPageBlock>
 
       <section slot="footer">
